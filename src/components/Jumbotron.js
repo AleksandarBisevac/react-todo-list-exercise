a62a010c6dd320a44116a40be68dd3a01acf3eb0
AleksandarBisevac <alek.bisevac@gmail.com>
2021-08-10 21:08:43 +0200

    first version of app

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+const Jumbotron = () =>{
+    return(
+        <div className="jumbotron text-center">
+            <h3>React ToDo</h3>
+        </div>
+    )
+}
+
+export default Jumbotron;
---
+++ src/components/NewToDo.js
@@ -0,0 +1,36 @@
+import React,{Component} from "react";
+
+class NewToDo extends Component{
+    state={
+        msg:"",
+        done: false,
+    }
+    setNewMsg = (e) => {
+        this.setState({
+            msg: e.target.value,
+        }
+        )
+    }
+    addTodo = () => {
+        this.props.addIntoTodos(this.state);
+        this.setState({msg:"",done:false});
+    }
+
+    render(){return(        
+    <div className="container">
+        <div className="row m-5">
+            <div className="col-8 offset-2">
+                <div className="input-group">
+                    <input onChange={this.setNewMsg} value={this.state.msg} type="text" className="form-control" placeholder="new message" />
+                    <div className="input-group-append">
+                        <button onClick={this.addTodo} className="btn btn-primary">Add</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>)
+
+    }
+}
+
+export default NewToDo;
